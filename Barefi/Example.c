c1516830085f55e8874ed286dde8a4ccfb59beba
Archer <125214627+ArcherPer@users.noreply.github.com>
2023-04-28 17:22:16 -0400

    Delete Barefi directory

--- Barefi/Example.c
+++
@@ -1,8 +0,0 @@
-#include "Uefi.h"
-
-EFI_STATUS efi_main() {
-    EFI_STATUS Status = gST->ConOut->OutputString(gST->ConOut,u"This is an example application\r\n");
-    return Status;
-}
-
-// clang -target x86_64-unknown-windows -ffreestanding -mno-red-zone -fno-stack-protector -fshort-wchar -c -o object.o Example.c
