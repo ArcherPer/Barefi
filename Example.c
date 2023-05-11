968f34522f6fa4ed0bcf003aa1f394e0b428cb54
Archer <125214627+ArcherPer@users.noreply.github.com>
2023-05-11 13:05:21 -0400

    Update Example.c

--- Example.c
+++ Example.c
@@ -4,5 +4,3 @@ EFI_STATUS efi_main() {
     EFI_STATUS Status = gST->ConOut->OutputString(gST->ConOut,u"This is an example application\r\n");
     return Status;
 }
-
-// clang -target x86_64-unknown-windows -ffreestanding -mno-red-zone -fno-stack-protector -fshort-wchar -c -o object.o Example.c
