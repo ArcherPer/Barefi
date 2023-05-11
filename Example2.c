0afaa0e6949b593303e0883755a3843712eb70b2
Archer <125214627+ArcherPer@users.noreply.github.com>
2023-05-11 13:08:17 -0400

    Add files via upload

--- Example2.c
+++ Example2.c
@@ -1,6 +1,6 @@
 #include "System/SystemTable.h" // Does not use the custom Barefi entry point
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+EFI_STATUS efi_entry_point(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     EFI_STATUS Status = SystemTable->ConOut->OutputString(SystemTable->ConOut,u"This is an example application\r\n");
     return Status;
 }
